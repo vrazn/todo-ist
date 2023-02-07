ebb1f99b06276a17759fd622a668ae272d945665
Vladislav Raznoschikov <vlad@grantit.io>
2023-02-08 04:55:04 +0800

    Enable CORS and switch default server port to 3001

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
+
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
