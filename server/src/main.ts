ae46c068bc558151dbe7dafbb935dd2e688b96fd
Vladislav Raznoschikov <vlad@grantit.io>
2023-02-13 13:01:58 +0800

    [Server] Performance & security optimisations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
+  await app.register(helmet);
 
   app.enableCors({ origin: '*' });
-
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3001);
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
